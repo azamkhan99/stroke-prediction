04a036be413a65a68b1a3ed5b717db034f742058
azamkhan <azamkhan@deloitte.co.uk>
2023-01-20 14:23:25 +0000

    added radio button to sidebar

--- Landing Page.py
+++ Landing Page.py
@@ -17,9 +17,24 @@ Begin by uploading a dataset."""
 
 # Sidebar setup
 st.sidebar.title("Sidebar")
-upload_file = st.sidebar.file_uploader("Upload a file containing data")
 
-# Check if file has been uploaded
-if upload_file is not None:
-    df = pd.read_csv(upload_file)
+use_sample_data = st.sidebar.radio(
+    "Dataset", ("Use Sample Data", "Upload a file containing data")
+)
+
+if use_sample_data == "Use Sample Data":
+    df = pd.read_csv("Data/healthcare-dataset-stroke-data.csv")
     st.session_state["df"] = df
+    if df is not None:
+        st.markdown("You may now navigate to Data Analysis Page!")
+
+elif use_sample_data == "Upload a file containing data":
+
+    upload_file = st.sidebar.file_uploader("")
+
+    # Check if file has been uploaded
+    if upload_file is not None:
+        df = pd.read_csv(upload_file)
+        st.session_state["df"] = df
+        if df is not None:
+            st.markdown("You may now navigate to Data Analysis Page!")
